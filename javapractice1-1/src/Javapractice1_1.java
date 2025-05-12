67247b2a904d949f4333eef1131abb37a4f21c46
ddt-yuya-kodama <ddt.yuya.kodama@gmail.com>
2025-05-12 14:00:27 +0900

    演習1-1

---
+++ javapractice1-1/src/Javapractice1_1.java
@@ -0,0 +1,30 @@
+/**
+ * クラス名:Javapractice1_1
+ * 概要    :演習1-1
+ * 作成者  :小玉有哉
+ * 作成日  :2025.05.12
+ */
+
+//クラス名Javapractice1_1
+public class Javapractice1_1 {
+	
+	 /**
+     * 関数名           :main
+     * メソッドの説明   :～～
+     * パラメータの説明 :なし
+     * 返り値           :なし
+     * 作成者           :小玉有哉
+     * 作成日           :2025.05.12
+     */
+	//mainメソッドの宣言
+	public static void main(String[] args) {
+		//；が無いためコンソールに初めてのjavaプログラムと表示されない
+		System.out.println("初めてのjavaプログラム")
+		//；が無いためコンソールに画面に出力していますと表示されない
+		System.out.println("画面に出力しています")
+
+	}
+
+}
+
+
