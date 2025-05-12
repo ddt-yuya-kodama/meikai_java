66c753bcf649c6d0416bfa18636242707bfea9a3
ddt-yuya-kodama <ddt.yuya.kodama@gmail.com>
2025-05-12 15:09:14 +0900

    演習1-1,1-2,1-3のメソッドの説明をすべてのファイルに追加修正

--- Javapractice1-3/src/Javapractice1_3.java
+++ Javapractice1-3/src/Javapractice1_3.java
@@ -11,7 +11,7 @@ public class Javapractice1_3 {
 	
 	 /**
      * 関数名           :main
-     * メソッドの説明   :～～
+     * メソッドの説明   :コンソールに改行文字を表示するメソッド
      * パラメータの説明 :なし
      * 返り値           :なし
      * 作成者           :小玉有哉
@@ -20,6 +20,7 @@ public class Javapractice1_3 {
 	//mainメソッドの宣言
 	public static void main(String[] args) {
 		//小玉有哉と小玉と有哉の間にスペースが空いて各文字一列ずつ表示される
+		//\n\nで一文字多く開けて改行
 		System.out.println("小\n玉\n\n有\n哉");
 
 	}
--- javapractice1-1/src/Javapractice1_1.java
+++ javapractice1-1/src/Javapractice1_1.java
@@ -5,22 +5,21 @@
  * 作成日  :2025.05.12
  */
 
-//クラス名Javapractice1_1
 public class Javapractice1_1 {
 	
 	 /**
      * 関数名           :main
-     * メソッドの説明   :～～
+     * メソッドの説明   :コンソールに文を表示
      * パラメータの説明 :なし
      * 返り値           :なし
      * 作成者           :小玉有哉
      * 作成日           :2025.05.12
      */
-	//mainメソッドの宣言
+
 	public static void main(String[] args) {
-		//；が無いためコンソールに初めてのjavaプログラムと表示されない
+		//；が無いためコンソールに初めてのjavaプログラムと表示される
 		System.out.println("初めてのjavaプログラム");
-		//；が無いためコンソールに画面に出力していますと表示されない
+		//；が無いためコンソールに画面に出力していますと表示される
 		System.out.println("画面に出力しています");
 
 	}
@@ -28,31 +27,3 @@ public class Javapractice1_1 {
 }
 
 
-/**
- * クラス名:Javapractice1_2
- * 概要    :演習1-1
- * 作成者  :小玉有哉
- * 作成日  :2025.05.12
- */
-
-//クラス名Javapractice1_2
-public class Javapractice1_2 {
-	
-	 /**
-     * 関数名           :main
-     * メソッドの説明   :～～
-     * パラメータの説明 :なし
-     * 返り値           :なし
-     * 作成者           :小玉有哉
-     * 作成日           :2025.05.12
-     */
-	//mainメソッドの宣言
-	public static void main(String[] args) {
-		//小玉有哉と各文字一列ずつ表示される
-		System.out.println("小\n玉\n有\n哉");
-		//小玉有哉と小玉と有哉の間にスペースが空いて各文字一列ずつ表示される
-		System.out.println("画面に出力しています");
-
-	}
-
-}
