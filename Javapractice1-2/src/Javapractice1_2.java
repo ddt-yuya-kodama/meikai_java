aa4a14c6a5675fe72cb90fe3d2fc0c889af70d39
ddt-yuya-kodama <ddt.yuya.kodama@gmail.com>
2025-05-12 14:41:57 +0900

    演習1-2

---
+++ Javapractice1-2/src/Javapractice1_2.java
@@ -0,0 +1,27 @@
+
+/**
+ * クラス名:Javapractice1_2
+ * 概要    :演習1-2
+ * 作成者  :小玉有哉
+ * 作成日  :2025.05.12
+ */
+
+//クラス名Javapractice1_2
+public class Javapractice1_2 {
+	
+	 /**
+     * 関数名           :main
+     * メソッドの説明   :～～
+     * パラメータの説明 :なし
+     * 返り値           :なし
+     * 作成者           :小玉有哉
+     * 作成日           :2025.05.12
+     */
+	//mainメソッドの宣言
+	public static void main(String[] args) {
+		//小玉有哉と小玉と有哉の間にスペースが空いて各文字一列ずつ表示される
+		System.out.println("小\n玉 \n有\n哉");
+
+	}
+
+}
